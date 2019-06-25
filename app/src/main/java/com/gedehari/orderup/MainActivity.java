9965b00ee914d0ea08b8b48519c5f18a185bb984
gedehari <harikresnagede@gmail.com>
2019-06-25 15:13:59 +0800

    Finally made a button to do something

--- app/src/main/java/com/gedehari/orderup/MainActivity.java
+++ app/src/main/java/com/gedehari/orderup/MainActivity.java
@@ -3,13 +3,34 @@ package com.gedehari.orderup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button order;
+
+    LinearLayout layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int i = 0;
+        layout = findViewById(R.id.layout);
+        order = findViewById(R.id.order);
+        order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(layout, "Test123", Snackbar.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
+
+
 }
