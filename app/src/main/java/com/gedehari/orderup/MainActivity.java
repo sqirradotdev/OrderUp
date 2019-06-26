23c57d331c90219bb847926be54b28f782b1ae16
gedehari <harikresnagede@gmail.com>
2019-06-27 06:15:42 +0800

    Fixed and added functionality to plus and minus buttons. Changed strings.

--- app/src/main/java/com/gedehari/orderup/MainActivity.java
+++ app/src/main/java/com/gedehari/orderup/MainActivity.java
@@ -1,36 +1,72 @@
 package com.gedehari.orderup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button order;
+    final String KEY_COUNT = "count ubah qty";
+
+    Button order, plus, minus;
+    TextView display;
 
     LinearLayout layout;
+
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) count = savedInstanceState.getInt(KEY_COUNT, 0);
+
         layout = findViewById(R.id.layout);
         order = findViewById(R.id.order);
+        plus = findViewById(R.id.plus);
+        minus = findViewById(R.id.minus);
+
+        display = findViewById(R.id.qtyDisplay);
+
         order.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(layout, "This indicates that the button is working.", Snackbar.LENGTH_SHORT).show();
-
+                Snackbar.make(layout, MainActivity.this.getString(R.string.button_working), Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                render();
+            }
+        });
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count--;
+                render();
             }
         });
 
+        render();
     }
 
+    void render() {
+        display.setText(String.valueOf(count));
+    }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(KEY_COUNT, count);
+        super.onSaveInstanceState(outState);
+    }
 }
