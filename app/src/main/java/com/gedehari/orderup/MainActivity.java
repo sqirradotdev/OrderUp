920f96525547682efdbd9c89c87b76a8f459f36e
gedehari <harikresnagede@gmail.com>
2019-06-27 21:04:43 +0800

    I screwed up. Added comments. Added dummy spinner, will change later

--- app/src/main/java/com/gedehari/orderup/MainActivity.java
+++ app/src/main/java/com/gedehari/orderup/MainActivity.java
@@ -7,27 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    final String KEY_COUNT = "count";
+    /* Final variables for savedInstanceState */
+    final String KEY_COUNT = "quantity";
 
+    /* Declaring activity objects */
     Button order, plus, minus;
     TextView display;
+    Spinner coffeetype = findViewById(R.id.spinner_coffeetype);
 
     LinearLayout layout;
 
-    int count = 0;
+    /* Quantity counter */
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState != null) count = savedInstanceState.getInt(KEY_COUNT, 0);
+        if (savedInstanceState != null) quantity = savedInstanceState.getInt(KEY_COUNT, 0);
 
         layout = findViewById(R.id.layout);
         order = findViewById(R.id.order);
@@ -47,14 +52,14 @@ public class MainActivity extends AppCompatActivity {
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                count++;
+                quantity++;
                 render();
             }
         });
         minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                count--;
+                quantity--;
                 render();
             }
         });
@@ -62,9 +67,10 @@ public class MainActivity extends AppCompatActivity {
         render();
     }
 
+    /* Update and render screen */
     void render() {
-        display.setText(String.valueOf(count));
-        minus.setEnabled(!isQtyZero(count));
+        display.setText(String.valueOf(quantity));
+        minus.setEnabled(!isQtyZero(quantity));
     }
 
     boolean isQtyZero(int q) {
@@ -73,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putInt(KEY_COUNT, count);
+        outState.putInt(KEY_COUNT, quantity);
         super.onSaveInstanceState(outState);
     }
 }
